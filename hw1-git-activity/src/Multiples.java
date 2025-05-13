5f78ba7462ad7561917c1936020864083a2b1657
Nathan Y <86241500+Nate-Yoon@users.noreply.github.com>
2025-05-13 16:38:21 -0400

    Delete hw1-git-activity/src/Multiples.java

--- hw1-git-activity/src/Multiples.java
+++
@@ -1,9 +0,0 @@
-public class Multiples {
-    public static void main(String[] args) {
-        for (int i = 1; i < 1000; i++) {
-            if (i % 3 == 0 || i % 5 == 0 ) {
-                System.out.println(i);
-            }
-        }
-    }
-}
